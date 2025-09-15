e47c0ae579ac9c85369522d408adc7f5dd06e580
Андрей Осьмаков <osmaav@yandex.ru>
2025-09-16 02:56:20 +0700

    добавил ключи в URL http://127.0.0.1:3000/?sntd&sva&hfa sva - показать правильные ответы; sntd - показать ссылку на норму; hfa - скрыть неверные ответы. Ключи можно задавать в любых сочетаниях и в любом порядке

--- docs/index.js
+++ docs/index.js
@@ -1,29 +1,57 @@
-// Выбор всех блоков вопросов
-const questions = document.querySelectorAll('.question');
+document.addEventListener('DOMContentLoaded', function () {
+  // Проверяем параметры в URL
+  // Получаем URL страницы
+  const urlString = window.location.href;
 
-// Проходим по каждому вопросу
-questions.forEach(function (questionBlock) {
-  // Поиск списка ответов
-  const answerItems = questionBlock.querySelectorAll('.question__answers-list-item');
+  // Создаем экземпляр URLSearchParams
+  const searchParams = new URLSearchParams(new URL(urlString).search);
 
-  // Перебираем каждый пункт ответа
-  answerItems.forEach(function (item) {
-    // Проверяем, помечен ли этот ответ как верный
-    const correctField = item.querySelector('input[name$=".correct"]');
-    const checkboxDiv = item.querySelector('.checkbox'); // Здесь будем менять класс div
+  // Читаем конкретные параметры
+  const selectValidAnswer = searchParams.has('sva');
+  const showNTD = searchParams.has('sntd');
+  const hideFailAnswers = searchParams.has('hfa');
 
-    if (correctField && correctField.value === 'true') {
-      // Ответ правильный, добавляем класс checkbox-green-checked
-      checkboxDiv.className = 'checkbox checkbox-green-checked';
-      // Ставим чекбокс на правильном ответе
-      const checkbox = item.querySelector('input[type="checkbox"]');
-      if (checkbox) {
-        checkbox.checked = true;
+  if (showNTD) {
+    document.querySelectorAll('div.row.ntdLink').forEach(el => el.removeAttribute('style'))
+  }
+  // Выбор всех блоков вопросов
+  const questions = document.querySelectorAll('.question');
+
+  // Проходим по каждому вопросу
+  questions.forEach(function (questionBlock) {
+    // Поиск списка ответов
+    const answerItems = questionBlock.querySelectorAll('.question__answers-list-item');
+
+    // Перебираем каждый пункт ответа
+    answerItems.forEach(function (item) {
+      // Проверяем, помечен ли этот ответ как верный
+      const correctField = item.querySelector('input[name$=".correct"]');
+      const checkboxDiv = item.querySelector('.checkbox'); // Контейнер с чекбоксом
+      if (correctField && correctField.value === 'true') {
+        checkboxDiv.className = 'checkbox checkbox-green';
+        // Если включёна sva, выделяем правильный ответ
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        if (selectValidAnswer) {
+          // Ставим чекбокс на правильном ответе
+          if (checkbox) {
+            checkbox.checked = true;
+          }
+        } else {
+          // Удаляем чекбокс на правильном ответе
+          if (checkbox) {
+            checkbox.checked = false;
+          }
+        }
+      } else {
+        // Если включён hfa, скрываем неправильные ответы
+        if (hideFailAnswers) {
+          item.setAttribute('style', 'display: none;');
+        } else {
+          // Иначе делаем красными неправильные ответы
+          checkboxDiv.className = 'checkbox checkbox-red';
+        }
       }
-    } else {
-      // Ответ неправильный, добавляем класс checkbox-red
-      checkboxDiv.className = 'checkbox checkbox-red';
-    }
+    });
   });
 });
 
