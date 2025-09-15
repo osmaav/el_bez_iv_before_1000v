9ec7933a5b5573f0966f3df81009dfce3fc793e4
Андрей Осьмаков <osmaav@yandex.ru>
2025-09-15 18:15:49 +0700

    добавил красный чек-бокс для не верных ответов и выделил верный ответ

--- docs/index.js
+++ docs/index.js
@@ -10,12 +10,20 @@ questions.forEach(function (questionBlock) {
   answerItems.forEach(function (item) {
     // Проверяем, помечен ли этот ответ как верный
     const correctField = item.querySelector('input[name$=".correct"]');
+    const checkboxDiv = item.querySelector('.checkbox'); // Здесь будем менять класс div
+
     if (correctField && correctField.value === 'true') {
+      // Ответ правильный, добавляем класс checkbox-green-checked
+      checkboxDiv.className = 'checkbox checkbox-green-checked';
       // Ставим чекбокс на правильном ответе
       const checkbox = item.querySelector('input[type="checkbox"]');
       if (checkbox) {
         checkbox.checked = true;
       }
+    } else {
+      // Ответ неправильный, добавляем класс checkbox-red
+      checkboxDiv.className = 'checkbox checkbox-red';
     }
   });
 });
+
