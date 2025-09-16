c3d1d5bc1e510e0aee3aa0dab77f6cfe947c9299
Андрей Осьмаков <osmaav@yandex.ru>
2025-09-17 01:01:36 +0700

    перенес задание параметров в шапку

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Проверяем состояние чекбоксов при загрузке страницы
   updateParametersFromCheckboxes();
 
-  // Назначаем слушатель события на изменение состояния чекбоксов
-  document.querySelectorAll('input[type="checkbox"]').forEach(input => {
+  // Назначаем слушатель события на изменение состояния чекбоксов в шапке
+  document.querySelectorAll('div.head input[type="checkbox"]').forEach(input => {
     input.addEventListener('change', updateParametersFromCheckboxes);
   });
 
@@ -23,46 +23,46 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
       ntdLinks.forEach(el => el.setAttribute('style', 'display: none;'));
     }
-    // Выбор всех блоков вопросов
-    const questions = document.querySelectorAll('.question');
+    // Поиск списка ответов
+    const answerItems = document.querySelectorAll('.question__answers-list-item');
 
-    // Проходим по каждому вопросу
-    questions.forEach(function (questionBlock) {
-      // Поиск списка ответов
-      const answerItems = questionBlock.querySelectorAll('.question__answers-list-item');
-
-      // Перебираем каждый пункт ответа
-      answerItems.forEach(function (item) {
-        // Проверяем, помечен ли этот ответ как верный
-        const correctField = item.querySelector('input[name$=".correct"]');
-        const checkboxDiv = item.querySelector('.checkbox'); // Контейнер с чекбоксом
-        if (correctField && correctField.value === 'true') {
-          checkboxDiv.className = 'checkbox checkbox-green';
-          // Если включёна sva, выделяем правильный ответ
-          const checkbox = item.querySelector('input[type="checkbox"]');
-          if (selectValidAnswer) {
-            // Ставим чекбокс на правильном ответе
-            if (checkbox) {
-              checkbox.checked = true;
-            }
-          } else {
-            // Удаляем чекбокс на правильном ответе
-            if (checkbox) {
-              checkbox.checked = false;
-            }
+    // Перебираем каждый пункт ответа
+    answerItems.forEach(function (item) {
+      // Считываем поле корректности ответа
+      const correctField = item.querySelector('input[name$=".correct"]');
+      // Контейнер с чекбоксом
+      const checkboxDiv = item.querySelector('div.checkbox');
+      // Проверяем, помечен ли этот ответ как верный
+      if (correctField && correctField.value === 'true') {
+        checkboxDiv.classList.add('green');
+        checkboxDiv.classList.remove('red');
+        // Если включёна sva, выделяем правильный ответ
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        if (selectValidAnswer) {
+          // Ставим чекбокс на правильном ответе
+          if (checkbox) {
+            checkbox.checked = true;
           }
         } else {
-          // Если включён hfa, скрываем неправильные ответы
-          if (hideFailAnswers) {
-            item.setAttribute('style', 'display: none;');
-          } else {
-            item.removeAttribute('style');
-            // Иначе делаем красными неправильные ответы
-            checkboxDiv.className = 'checkbox checkbox-red';
+          // Удаляем чекбокс на правильном ответе
+          if (checkbox) {
+            checkbox.checked = false;
           }
-        };
-      });
+        }
+      } else {
+        if (correctField && correctField.value === 'false') {
+          // Иначе делаем красными неправильные ответы
+          checkboxDiv.classList.add('red');
+          checkboxDiv.classList.remove('green');
+        }
+        // Если включён hfa, скрываем неправильные ответы
+        if (hideFailAnswers) {
+          item.setAttribute('style', 'display: none;');
+        } else {
+          item.removeAttribute('style');
+        }
+      };
     });
-  };
+  }
 });
 
